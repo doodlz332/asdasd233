7bda4a460dc88b6e871ab2c8c2dcc1abdfbde324
doodlz332 <emillaur1999@gmail.com>
2025-10-09 14:21:08 +0300

    Add files via upload

--- Harjutus1.py
+++ Harjutus1.py
@@ -1,1 +1,31 @@
-print("Tere maailm!")
+import turtle
+
+#liikumine
+turtle.penup()#pliiats üles
+turtle.goto(-400,200)#asukoht
+turtle.pendown()#pliiats alla
+#kolmnurk
+for i in range (3): #tsükkel x3
+    turtle.fd(200) #pikkus forward
+    turtle.left(120) #kraadid väliskülg
+
+turtle.penup()
+turtle.goto(-40,375)
+turtle.pendown()
+t=turtle
+
+t.forward(100)
+t.backward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.backward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+
+
+turtle.done()
