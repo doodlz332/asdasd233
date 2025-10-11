a3a97cac49153a294aeb0cfd5eaabdcd82896609
doodlz332 <emillaur1999@gmail.com>
2025-10-11 13:51:13 +0300

    asd
    asd

--- Harjutus5.py
+++ Harjutus5.py
@@ -1,5 +1,23 @@
 import random
 import turtle
+
+# Kasuta Python Turtles’i, et joonistada stseen, kus redel toetub majale 53° nurga all. Redeli ülemine ots peaks ulatuma 4,4 meetri kõrgusele maja seinal virtuaalses mõõtkavas.
+# Muuda nurgad radiaanideks (radian)
+# Redeli kaugus seinast: kasuta tangensfunktsiooni (tan) ja antud nurka, et leida, kui kaugele redeli alumine ots on seinast. Valem:
+# Kaugus=Ko˜rgus seinaltan⁡(Nurk) 
+# Redeli pikkus: kasuta Pythagorase teoreemi, et leida redeli pikkus, kui tead redeli kõrgust seinal ja kaugust seinast. Valem:
+# Pikkus=(Ko˜rgus seinal)2+(Kaugus seinast)2 
+# Ümarda vastus 2 komakohta
+# Kuva vastused konsoolid
+# Kasuta Python Turtles’i, et visualiseerida maantee tõusu, mis 140 meetri jooksul tõuseb 15 meetrit.
+# Sinu ülesandeks on arvutada maantee tõusunurk ja näidata, kui suurt tõusu protsentides liiklusmärk enne tõusu kuvaks.
+# Tõusunurga arvutamine: Kasuta tangensfunktsiooni inversi (atan), et leida tõusunurk radiaanides, ja teisenda see kraadideks. Nurk (kraadides) = atan(tõus/maa) × (180/π).
+# Tõusu protsent: Tõusu protsent on (vertikaalne tõus / horisontaalne vahemaa) × 100.
+# Ümarda täisarvuti ja kuva vastus konsoolis
+ 
+ 
+ 
+ turtle.done()
 # Mündiviskamise äraarvamine koos juhuslikkusega (and ja or)
 # Kirjuta programm, mis simuleerib mündiviskamist. Programm genereerib juhusliku tulemuse – “kiri” või “kull”, kasutades random.randint(0,1) funktsiooni.
 # Programmi koostamisel pead importima import random mooduli ja kasutama randint() funktsiooni, et valida kahe võimaliku tulemuse vahel.
