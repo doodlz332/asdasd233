b7eef72a283e6d431ed6351515af4228ad71a2d2
doodlz332 <emillaur1999@gmail.com>
2025-11-06 20:50:29 +0200

    Create Kodutöö1.py
    sdds

---
+++ Kodutöö1.py
@@ -0,0 +1,38 @@
+#Harjutus 1.1
+print ("Tere, maailm!")
+
+#Harjutus 1.2
+aasta = 2020
+liblikas = "teelehe-mosambiikliblikas"
+lause_keskosa = ". aasta liblikas on "
+lause = str(aasta) + lause_keskosa + liblikas
+print(lause)
+
+#Harjutus 1.3
+korgus = float(input("Sisesta pilvede aluse kõrgus (km): "))
+
+if korgus > 6:
+    print("Need on ülemised pilved.")
+elif 2 <= korgus <= 6:
+    print("Need on keskmised pilved.")
+else:
+    print("Need on alumised pilved.")
+    
+#Harjutus 1.4
+inimesi = int(input("Sisesta inimeste arv: "))
+kohad = int(input("Sisesta ühe bussi kohtade arv: "))
+
+busside_arv = inimesi // kohad
+ulejaanud = inimesi % kohad
+
+if ulejaanud > 0:
+    busside_arv += 1
+    viimases_bussis = ulejaanud
+else:
+    viimases_bussis = kohad
+
+print("Inimeste arv:", inimesi)
+print("Kohtade arv:", kohad)
+print("Busse vaja:", busside_arv)
+print("Viimases bussis inimesi:", viimases_bussis)
+
