21b774321633749e7a4d63f51354824721991ef4
doodlz332 <emillaur1999@gmail.com>
2025-11-06 13:42:34 +0200

    Add files via upload

---
+++ Harjutus11.py
@@ -0,0 +1,8 @@
+#Kirjuta funktsioon pikim_sona(), mis võtab sisendiks sõnade listi ja tagastab pikima sõna pikkuse. Prindi tulemus konsooliaknasse.
+#H1
+
+def tervita():
+    print("Tere Maailm!")
+
+def tervita2():
+    return("Tere Kosmos!")
